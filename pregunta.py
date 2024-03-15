72f69aedf58ce10dc1f886ebb1d5ffa750e9dfee
anfrestrepora <anfrestrepora@unal.edu.co>
2024-03-15 15:27:03 -0500

    Intento2

--- pregunta.py
+++ pregunta.py
@@ -11,32 +11,22 @@ import pandas as pd
 
 def clean_data(df):
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0) # El index_col=0 permite que no se genere una columna adicional en el df xD
         # Eliminar filas con datos faltantes
     df = df.dropna()
-    
-
+    #limpieza principales caracteres y conversion a minusculas
+    df=df.apply(lambda x: x.astype(str))
+    df=df.apply(lambda x: x.str.replace(",", ""))
+    df=df.apply(lambda x: x.str.replace("$", ""))
+    df=df.apply(lambda x: x.str.replace("_", " "))
+    df=df.apply(lambda x: x.str.replace("-", " "))
+    df=df.apply(lambda x: x.str.lower())
     # Convertir la columna 'fecha_de_beneficio' al formato de fecha adecuado
     df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], errors='coerce')
-
     # Limpiar los valores de la columna 'monto_del_credito'
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '').str.replace(',', '').astype(float)
-
-    # Limpiar los valores de las columnas 'sexo', 'tipo_de_emprendimiento' y 'idea_negocio'
-    df['sexo'] = df['sexo'].str.lower()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
-    df['idea_negocio'] = df['idea_negocio'].str.lower()
-
-    # Asegurarse de que los valores de las columnas 'barrio', 'estrato', 'comuna_ciudadano' y 'línea_credito' sean adecuados
-    df['barrio'] = df['barrio'].str.strip()
-    df['estrato'] = pd.to_numeric(df['estrato'], errors='coerce')
-    df['comuna_ciudadano'] = pd.to_numeric(df['comuna_ciudadano'], errors='coerce')
-    df['línea_credito'] = df['línea_credito'].str.lower()
-
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float)
     # Eliminar filas duplicadas
     df = df.drop_duplicates()
-
-
     return df
 
 
